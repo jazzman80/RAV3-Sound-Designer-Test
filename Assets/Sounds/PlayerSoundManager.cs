f1d3e60cb0ad153c66212643c780112c9bff9ca6
jazzman80 <77989227+jazzman80@users.noreply.github.com>
2021-03-30 21:09:40 +0300

    Test

---
+++ Assets/Sounds/PlayerSoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class PlayerSoundManager : MonoBehaviour
+{
+    [Header("Sounds")]
+    [SerializeField] StudioEventEmitter footsteps;
+    [SerializeField] StudioEventEmitter jump;
+
+    private Vector3 lastPosition;
+    private FMOD.Studio.Bus footbus;
+    private string footstring = "Bus:/Footbus";
+    private bool isActive;
+
+    private void Start()
+    {
+        isActive = false;
+        footbus = RuntimeManager.GetBus(footstring);
+        footbus.setVolume(0.0001f);
+        lastPosition = transform.position;
+        footsteps.Play();
+        Invoke("Activate", 1.0f);
+    }
+
+    private void Update()
+    {
+        if (isActive) footbus.setVolume(Speed() * 0.05f);
+
+        if (Input.GetButtonDown("Jump")) jump.Play();
+
+        lastPosition = transform.position;
+    }
+
+    private float Speed()
+    {
+        Vector3 distance = lastPosition - transform.position;
+        return distance.magnitude / Time.deltaTime;
+    }
+
+    private void Activate()
+    {
+        isActive = true;
+    }
+}
